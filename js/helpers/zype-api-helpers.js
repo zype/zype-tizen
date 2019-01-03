0cef81a1e937ba3a19212b222ce52f369560679a
andyzheng547 <andy.zheng249@gmail.com>
2019-01-03 16:29:11 -0500

    Add call for sliders. New view controller and view

--- js/helpers/zype-api-helpers.js
+++ js/helpers/zype-api-helpers.js
@@ -1,105 +1,104 @@
 (function(exports){
-    "use strict";
+  "use strict";
 
-    let ZypeApiHelpers = function(){
-        let _this = this;
+  let ZypeApiHelpers = function(){
+    let _this = this;
 
-        this.getPlaylistChildren = (zypeApi, playlistId) => {
-            return new Promise((resolve, reject) => {
-                let params = {
-                  parent_id: playlistId,
-                  per_page: 50,
-                  sort: "priority",
-                  order: "dsc",
-                  page: 1
-                };
+    this.getPlaylistChildren = (zypeApi, playlistId) => {
+      return new Promise((resolve, reject) => {
+        let params = {
+          parent_id: playlistId,
+          per_page: 50,
+          sort: "priority",
+          order: "dsc",
+          page: 1
+        };
 
-                zypeApi.getPlaylists(params)
-                .then(
-                    (playlistsResp) => {
-                        let playlistChildrenArray = [];
+        zypeApi.getPlaylists(params)
+        .then(
+          (playlistsResp) => {
+            let playlistChildrenArray = [];
 
-                        if (playlistsResp && playlistsResp.response.length > 0) {
-                            let playlists = playlistsResp.response;
+            if (playlistsResp && playlistsResp.response.length > 0) {
+              let playlists = playlistsResp.response;
 
-                            let functionCallsArray = [];
-                            for (let i = 0; i < playlists.length; i++) {
-                                // video playlist
-                                if (playlists[i].playlist_item_count > 0){
-                                    functionCallsArray.push([
-                                        zypeApi.getPlaylistVideos,
-                                        [ playlists[i]._id, {per_page: 50} ]
-                                    ]);
+              let functionCallsArray = [];
+              for (let i = 0; i < playlists.length; i++) {
+                // video playlist
+                if (playlists[i].playlist_item_count > 0){
+                  functionCallsArray.push([
+                    zypeApi.getPlaylistVideos,
+                    [ playlists[i]._id, {per_page: 50} ]
+                  ]);
 
-                                    playlistChildrenArray.push({
-                                        id: playlists[i]._id,
-                                        type: "videos",
-                                        title: playlists[i].title,
-                                        thumbnailLayout: playlists[i].thumbnail_layout
-                                    });
-                                }
-                                // playlist of playlists
-                                else {
-                                    functionCallsArray.push([
-                                        zypeApi.getPlaylists,
-                                        [ {parent_id: playlists[i]._id, per_page: 50, sort: "priority", order: "dsc"} ]
-                                    ]);
+                  playlistChildrenArray.push({
+                    id: playlists[i]._id,
+                    type: "videos",
+                    title: playlists[i].title,
+                    thumbnailLayout: playlists[i].thumbnail_layout
+                  });
+                }
+                // playlist of playlists
+                else {
+                  functionCallsArray.push([
+                    zypeApi.getPlaylists,
+                    [ {parent_id: playlists[i]._id, per_page: 50, sort: "priority", order: "dsc"} ]
+                  ]);
 
-                                    playlistChildrenArray.push({
-                                        id: playlists[i]._id,
-                                        type: "playlists",
-                                        title: playlists[i].title,
-                                        thumbnailLayout: playlists[i].thumbnail_layout
-                                    })
-                                }
-                            }
+                  playlistChildrenArray.push({
+                    id: playlists[i]._id,
+                    type: "playlists",
+                    title: playlists[i].title,
+                    thumbnailLayout: playlists[i].thumbnail_layout
+                  })
+                }
+              }
 
-                            zypeApi.callMultiple(functionCallsArray)
-                            .then(
-                                (resps) => {
-                                    for (let i = 0; i < playlistChildrenArray.length; i++) {
-                                        playlistChildrenArray[i].content = resps[i].response;
-                                    }
-          
-                                    resolve(playlistChildrenArray)
-                                },
-                                (err) => {
-                                    reject(err);
-                                }
-                            );
+              zypeApi.callMultiple(functionCallsArray)
+              .then(
+                (resps) => {
+                  for (let i = 0; i < playlistChildrenArray.length; i++) {
+                    playlistChildrenArray[i].content = resps[i].response;
+                  }
 
+                  resolve(playlistChildrenArray)
+                },
+                (err) => {
+                  reject(err);
+                }
+              );
 
-                        } 
-                        else {
-                            zypeApi.getPlaylist(playlistId, {})
-                            .then(
-                                (playlistResp) => {
-                                    let playlist = playlistResp.response;
+            }
+            else {
+              zypeApi.getPlaylist(playlistId, {})
+              .then(
+                (playlistResp) => {
+                  let playlist = playlistResp.response;
 
-                                    zypeApi.getPlaylistVideos(playlistId, {per_page: 50})
-                                    .then(
-                                        (resp) => {
-                                            playlistChildrenArray.push({
-                                                type: "videos",
-                                                title: playlist.title,
-                                                thumbnailLayout: playlist.thumbnail_layout,
-                                                content: resp.response
-                                            });
+                  zypeApi.getPlaylistVideos(playlistId, {per_page: 50})
+                  .then(
+                    (resp) => {
+                      playlistChildrenArray.push({
+                        type: "videos",
+                        title: playlist.title,
+                        thumbnailLayout: playlist.thumbnail_layout,
+                        content: resp.response
+                      });
 
-                                            resolve(playlistChildrenArray);
-                                        },
-                                        (err) => { reject(err); }
-                                    );
-                                },
-                                (err) => { reject(err); }
-                            );
-                        }
+                      resolve(playlistChildrenArray);
                     },
                     (err) => { reject(err); }
-                );
-            });
-        };
-    };
+                  );
+                },
+                (err) => { reject(err); }
+              );
+            }
+          },
+          (err) => { reject(err); }
+        );
+      });
+    }; // getPlaylistChildren()
+  };
 
-    exports.ZypeApiHelpers = new ZypeApiHelpers();
+  exports.ZypeApiHelpers = new ZypeApiHelpers();
 })(window);
