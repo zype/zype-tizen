9f9ebda588a1055adb3c8ed17a7ab4288430b978
andyzheng547 <andy.zheng249@gmail.com>
2019-02-01 21:54:17 -0500

    Updated UI for favorites screen

--- js/views/FavoritesView.js
+++ js/views/FavoritesView.js
@@ -12,9 +12,12 @@
     let _this = this;
 
     // MARK: - HTML ids
-    const templateId = "#favorites-view-template";
-    const favoritesContainerId = "#favorites-container";
-    const favoritesHolderClass = " .favorites-holder";
+    const templateId = "#favorites-view-template"; // template for favorites view
+    const videosTemplateId = "#favorite-videos-template"; // template for favorite videos
+    const favoritesContainerId = "#favorites-container"; // div to append favorites view to
+    const videosContainerClass =  " .video-favorites-container";
+    const videoClass = " .video";
+
     const favoritesInfoClass = " .favorites-info";
     const signInBtnClass = " .sign-in-button";
     const helperMsgClass = " .helper-message";
@@ -23,6 +26,8 @@
 
     this.favorites = null; // array of video objects
     this.signedIn = null;  // bool
+
+    this.index = null; // position of focuses favorite
 
     /**
      * Initialization
@@ -38,6 +43,9 @@
           classes: { theme: appDefaults.theme },
           brandColor: appDefaults.brandColor,
           ids: { id: id }
+        },
+        labels: {
+          signInButton: appDefaults.labels.signInButton
         }
       };
 
@@ -46,18 +54,45 @@
       $(favoritesContainerId).append(renderedTemplate);
 
       this.setFavorites(args.favorites);
-
-      debugger;
     };
 
     /**
      * Private methods
      */
-    const showFavorites = () => $(this.id + favoritesHolderClass).removeClass("invisible");
-    const hideFavorites = () => $(this.id + favoritesHolderClass).addClass("invisible");
+    const showFavorites = () => $(this.id + videosContainerClass).removeClass("invisible");
+    const hideFavorites = () => $(this.id + videosContainerClass).addClass("invisible");
 
     const showInfo = () => $(this.id + favoritesInfoClass).removeClass("invisible");
     const hideInfo = () => $(this.id + favoritesInfoClass).addClass("invisible");
+
+    const formattedVideos = videos => {
+      let formattedVids = videos.map( vid => {
+        return {
+          id: vid._id,
+          title: vid.title,
+          description: vid.description,
+          duration: vid.duration || 0,
+          onAir: vid.on_air,
+          subscriptionRequired: vid.subscription_required,
+          imgUrl: vid.thumbnails[0].url || ""
+        }
+      });
+
+      return formattedVids;
+    };
+
+    const updateFavoriteVideos = videos => {
+      const context = {
+        videos: formattedVideos(videos),
+        css: {
+          classes: { theme: appDefaults.theme }
+        }
+      };
+
+      let template = $(videosTemplateId);
+      let renderedTemplate = Utils.buildTemplate(template, context);
+      $(this.id + videosContainerClass).html(renderedTemplate);
+    };
 
     /**
      * Update view
@@ -71,6 +106,9 @@
         if (favs.length > 0) {
           showFavorites();
           hideInfo();
+          updateFavoriteVideos(favs);
+
+          this.setFocusedVideo(0);
         } else {
           hideFavorites();
           this.showFavoritesHelperMsg();
@@ -95,6 +133,35 @@
       $(this.id + signInBtnClass).addClass("invisible");
 
       showInfo();
+    };
+
+    // setFocusedVideo() sets selected video index and focuses video thumbnail
+    this.setFocusedVideo = index => {
+      if (this.favorites[index]) {
+        this.index = index;
+        this.unfocusVideos();
+        this.focusVideo();
+        this.moveVideoContainer();
+      }
+    };
+
+    // Update videos
+    this.unfocusVideos = () => $(this.id + videoClass).removeClass("focused");
+    this.focusVideo = () => {
+      let currentVideo = $(this.id).find(videoClass)[this.index];
+      $(currentVideo).addClass("focused");
+    };
+    this.moveVideoContainer = () => { // Note: requires this.videoIndex to be set
+      let videosContainer = $(this.id + videosContainerClass)[0];
+
+      let video = $(this.id).find(videoClass)[0];
+      let videoThumbnailWidth = $(video).width();
+      let videoMargin = videoThumbnailWidth * 0.10;
+      let newLeft = -(this.videoIndex * (videoThumbnailWidth + videoMargin));
+
+      $(videosContainer).animate({
+        "left": String(newLeft) + "px"
+      }, 200);
     };
 
     // MARK: - Update view state
